f33b6c552528eb16d2a95252e99605143cfc2982
Joe Schweiss <joschw@Joes-MacBook-Pro.local>
2023-04-18 17:09:42 -0400

    updates to many .py files, added 2009 and 2020 datasets

---
+++ src/cleanup_2020.py
@@ -0,0 +1,197 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import geopandas as gpd
+import seaborn as sb
+
+fpath = "data/spotify_top_100_2010_2019.csv"
+df = pd.read_csv(fpath)
+
+df = df.dropna()
+
+col_val = []
+for x in range(len(df['genres'])):
+  temp = []
+  temp =(df['genres'][x].lstrip("['").rstrip("'])").split("', '"))
+  col_val.append(temp[0])
+df['top genre'] = col_val
+del df['genres']
+
+df['duration_ms'] = df['duration_ms'].div(60000)
+
+# variable to hold the count
+cnt = 0
+  
+# list to hold visited values
+visited = []
+  
+# loop for counting the unique
+# values in height
+for i in range(0, len(df['top genre'])):
+    
+    if df['top genre'][i] not in visited: 
+        
+        visited.append(df['top genre'][i])
+          
+        cnt += 1
+  
+print("No.of.unique values :",
+      cnt)
+  
+print("unique values :",
+      visited)
+
+#limit the number of genres
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'hip hop' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'rap' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'afroswing' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'g funk' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'drill' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'latin' if 'latin' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'latin' if 'sertanejo' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'latin' if 'reggaeton' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'pop' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'idol' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'talent' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'boy' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'wave' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'alt z' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'complex' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'hollywood' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'adult standards' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'reggae fusion' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'glee' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'rock' if 'rock' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'rock' if 'metal' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'rock' if 'emo' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'indie' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'shoe' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'downtem' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'psych' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'stomp' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'r&b' if 'r&b' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'r&b' if 'soul' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'r&b' if 'afrofut' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'house' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'dance' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'lilith' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'big room' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'grime' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'edm' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'techno' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'basshall' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'clubbing' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'bro' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'electro' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'aussietr' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'comic' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'new french' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'trance' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'covertronica' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'singer-songwriter' if 'mellow' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'singer-songwriter' if 'songwriter' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'contemporary country' if 'black americana' in x else x)
+
+#print(len(df['top genre'].unique()))
+del df['entry']
+df["top k"] = 1
+df.loc[df["ranking"] > 25, "top k"] = 0
+df = df[['top year', 'top k', 'ranking', 'artist', 'track', 'release year', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'key', 'mode', 'duration_ms', 'popularity', 'top genre']]
+df.to_csv("data/top_100_cleaned.csv")
+
+sb.scatterplot(data = df, x="ranking", y="liveness")
+
+fpath = "data/spotify_top_50_2020.csv"
+df = pd.read_csv(fpath)
+
+df = df.dropna()
+
+col_val = []
+for x in range(len(df['genres'])):
+  temp = []
+  temp =(df['genres'][x].lstrip("['").rstrip("'])").split("', '"))
+  col_val.append(temp[0])
+df['top genre'] = col_val
+del df['genres']
+
+df['duration_ms'] = df['duration_ms'].div(60000)
+
+# variable to hold the count
+cnt = 0
+  
+# list to hold visited values
+visited = []
+  
+# loop for counting the unique
+# values in height
+for i in range(0, len(df['top genre'])):
+    
+    if df['top genre'][i] not in visited: 
+        
+        visited.append(df['top genre'][i])
+          
+        cnt += 1
+  
+print("No.of.unique values :",
+      cnt)
+  
+print("unique values :",
+      visited)
+
+#limit the number of genres
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'hip hop' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'rap' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'afroswing' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'g funk' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'hip hop' if 'drill' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'latin' if 'latin' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'latin' if 'sertanejo' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'latin' if 'reggaeton' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'pop' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'idol' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'talent' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'boy' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'wave' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'alt z' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'complex' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'hollywood' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'adult standards' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'reggae fusion' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'pop' if 'glee' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'rock' if 'rock' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'rock' if 'metal' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'rock' if 'emo' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'indie' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'shoe' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'downtem' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'psych' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'indie' if 'stomp' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'r&b' if 'r&b' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'r&b' if 'soul' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'r&b' if 'afrofut' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'house' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'dance' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'lilith' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'big room' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'grime' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'edm' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'techno' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'basshall' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'clubbing' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'bro' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'electro' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'aussietr' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'comic' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'new french' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'trance' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'dance' if 'covertronica' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'singer-songwriter' if 'mellow' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'singer-songwriter' if 'songwriter' in x else x)
+df['top genre'] = df['top genre'].apply(lambda x: 'contemporary country' if 'black americana' in x else x)
+
+#print(len(df['top genre'].unique()))
+del df['entry']
+df["top k"] = 1
+df.loc[df["ranking"] > 25, "top k"] = 0
+df = df[['top year', 'top k', 'ranking', 'artist', 'track', 'release year', 'danceability', 'energy', 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'key', 'mode', 'duration_ms', 'popularity', 'top genre']]
+df.to_csv("data/2020_top50_cleaned.csv")
---
+++ src/logreg_norm.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 26 13:19:33 2023
+
+@author: joschw
+"""
+
+from numpy.linalg import linalg
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from sklearn.preprocessing import minmax_scale
+
+
+# load the data set and convert to numpy array
+# this is a data set on colleges
+# target variable: Private: Yes or No
+origdat = pd.read_csv("data/top_100_cleaned.csv", index_col=[0])
+del origdat['top genre']
+origdat["top k"] = 1
+origdat.loc[origdat["ranking"] > 25, "top k"] = 0
+cols = origdat.select_dtypes(np.number).columns
+origdat[cols] = minmax_scale(origdat[cols])
+dat = origdat.to_numpy()
+# the binary target variable happens to be in
+# in the second column of my data set
+y = dat[:,1]
+# the predictor variables are the remaining
+# columns of the data set
+Xdf = origdat.iloc[:,6:]
+X = dat[:,6:].astype('float')
+
+def logis(x):
+    # logistic function = e^x/(1+e^x)
+    x = x.astype('float')
+    return 1/(1+np.exp(-x))
+
+def logreg(y,X,tol=1e-8):
+    # logistic regression function:
+        # method:
+        # Convex Optimization via Newton's Iteration
+        # uses:
+        # when the target variable is a binary 0/1 variable
+        # conditions:
+        # n > p
+        # input:
+        # y = target variable (n x 1 numpy array)
+        # X = design matrix (n x (p+1) numpy array)
+        # tol = tolerance (used in stoping crition)
+        # output:
+        # beta = estimated logistic regression coefficients
+    # n = sample size
+    # p = number of predictor variables
+    n,p = np.shape(X)
+    # add the columns of one (for the intercept term)
+    # to the design matrix X
+    X = np.column_stack((np.ones(n),X))
+    Xt = np.transpose(X)
+    beta = np.zeros(p+1)    # initialize beta = (0,0,...,0)
+    lin = np.matmul(X,beta) # linear predictor
+    yhat = logis(lin) # estimated probability of the event y=1
+    grad = -np.matmul(Xt,y-yhat) # gradient of logistic loss function
+    yhatvar = np.multiply(yhat,1-yhat) # estimated variance of y 
+    H = np.matmul(Xt*yhatvar,X) # Hessian matrix of logistic loss function
+    beta = beta-np.matmul(np.linalg.inv(H),grad) # update beta vector
+    while np.max(np.abs(grad))>tol and np.max(yhatvar)>tol:
+        # print (maximum abs value in gradient vector
+        # once this value is <tol then the while loop terminates
+        print(np.max(np.abs(grad)))
+        lin = np.matmul(X,beta) # linear predictor
+        yhat = logis(lin) # estimated probability of the event y=1
+        grad = -np.matmul(Xt,y-yhat) # gradient of logistic loss function
+        yhatvar = np.multiply(yhat,1-yhat) # estimated variance of y 
+        H = np.matmul(Xt*yhatvar,X) # Hessian matrix of logistic loss function
+        Hinv = np.linalg.inv(H) # inverse of Hessian
+        beta = beta-np.matmul(Hinv,grad) # update beta vector
+    zscore = np.divide(beta,np.sqrt(np.diag(Hinv))) # z-score of beta estimates
+    return beta,zscore
+
+
+
+betahat,zscore = logreg(y,X)
+# betahat = estimated logistic regression coefficients
+# zscore = these are betahat/sqrt(var(betahat)) ~ N(0,1)
+#   under the assumption that the true beta = 0
+
+# Hypothesis testing:
+# Question: Is variable x_j a useful predictor
+#   of y when all other predictor variables
+#   x_0, x_1, x_{j-1}, ..., x_{j+1}, ..., , x_{p-1} are
+#   included in the model?
+
+# For which variables is the answer to the above
+# question equal to "yes"?
+Xdf.columns[np.where(np.abs(zscore[1:])>1.96)]
+print(Xdf.columns[np.where(np.abs(zscore[1:])>1.96)])
+
+# logistic function plot
+linhat = betahat[0]+np.matmul(X,betahat[1:])
+plt.plot(linhat,y,'k.',markersize=3)
+x = np.linspace(np.floor(np.min(linhat)),np.ceil(np.max(linhat)),100)
+plt.plot(x,logis(x))
+
+## confusion matrix
+# Interpretation:
+    # conf[0,0] = proportion of obs correctly
+    #   classified as y=1 when true value is y=1
+    # conf[0,1] = proportion of obs incorrectly
+    #   classified as y=1 when true value is y=0
+    # conf[1,0] = proportion of obs incorrectly
+    #   classified as y=0 when true value is y=1
+    # conf[1,1] = proportion of obs correctly
+    #   classified as y=0 when true value is y=0
+conf = np.zeros([2,2])
+conf[0,0] = np.mean(np.logical_and(linhat>0,y==1))
+conf[0,1] = np.mean(np.logical_and(linhat>0,y==0))
+conf[1,0] = np.mean(np.logical_and(linhat<=0,y==1))
+conf[1,1] = np.mean(np.logical_and(linhat<=0,y==0))
+print(np.round(conf,3))
+
+
+origdat2 = pd.read_csv("data/2009_top100_cleaned.csv", index_col=[0])
+del origdat2['top genre']
+origdat2["top k"] = 1
+origdat2.loc[origdat2["ranking"] > 25, "top k"] = 0
+cols = origdat2.select_dtypes(np.number).columns
+origdat2[cols] = minmax_scale(origdat2[cols])
+dat2 = origdat2.to_numpy()
+# the binary target variable happens to be in
+# in the second column of my data set
+y2 = dat2[:,1]
+# the predictor variables are the remaining
+# columns of the data set
+Xdf2 = origdat2.iloc[:,6:]
+X2= dat2[:,6:].astype('float')
+
+linhat2 = betahat[0]+np.matmul(X2,betahat[1:])
+plt.plot(linhat2,y2,'k.',markersize=3)
+x2 = np.linspace(np.floor(np.min(linhat2)),np.ceil(np.max(linhat2)),100)
+plt.plot(x2,logis(x2))
+plt.xlim([-10,10])
+plt.grid(which='major', axis='x')
+plt.title('Plot of 2009 actual rankings versus score')
+plt.show()
+plt.close()
+
+## confusion matrix
+# Interpretation:
+    # conf[0,0] = proportion of obs correctly
+    #   classified as y=1 when true value is y=1
+    # conf[0,1] = proportion of obs incorrectly
+    #   classified as y=1 when true value is y=0
+    # conf[1,0] = proportion of obs incorrectly
+    #   classified as y=0 when true value is y=1
+    # conf[1,1] = proportion of obs correctly
+    #   classified as y=0 when true value is y=0
+conf = np.zeros([2,2])
+conf[0,0] = np.mean(np.logical_and(linhat2>0,y2==1))
+conf[0,1] = np.mean(np.logical_and(linhat2>0,y2==0))
+conf[1,0] = np.mean(np.logical_and(linhat2<=0,y2==1))
+conf[1,1] = np.mean(np.logical_and(linhat2<=0,y2==0))
+print(np.round(conf,3))
+
+origdat3 = pd.read_csv("data/2020_top50_cleaned.csv", index_col=[0])
+del origdat3['top genre']
+origdat3["top k"] = 1
+origdat3.loc[origdat3["ranking"] > 25, "top k"] = 0
+cols = origdat3.select_dtypes(np.number).columns
+origdat3[cols] = minmax_scale(origdat3[cols])
+dat3 = origdat3.to_numpy()
+# the binary target variable happens to be in
+# in the second column of my data set
+y3 = dat3[:,1]
+# the predictor variables are the remaining
+# columns of the data set
+Xdf3 = origdat3.iloc[:,6:]
+X3= dat3[:,6:].astype('float')
+
+linhat3 = betahat[0]+np.matmul(X3,betahat[1:])
+plt.plot(linhat3,y3,'k.',markersize=3)
+x3 = np.linspace(np.floor(np.min(linhat3)),np.ceil(np.max(linhat3)),50)
+plt.plot(x3,logis(x3))
+plt.xlim([-8,8])
+plt.grid(which='major', axis='x')
+plt.title('Plot of 2020 actual rankings versus score')
+plt.show()
+plt.close()
+
+
+## confusion matrix
+# Interpretation:
+    # conf[0,0] = proportion of obs correctly
+    #   classified as y=1 when true value is y=1
+    # conf[0,1] = proportion of obs incorrectly
+    #   classified as y=1 when true value is y=0
+    # conf[1,0] = proportion of obs incorrectly
+    #   classified as y=0 when true value is y=1
+    # conf[1,1] = proportion of obs correctly
+    #   classified as y=0 when true value is y=0
+conf = np.zeros([2,2])
+conf[0,0] = np.mean(np.logical_and(linhat3>0,y3==1))
+conf[0,1] = np.mean(np.logical_and(linhat3>0,y3==0))
+conf[1,0] = np.mean(np.logical_and(linhat3<=0,y3==1))
+conf[1,1] = np.mean(np.logical_and(linhat3<=0,y3==0))
+print(np.round(conf,2))
